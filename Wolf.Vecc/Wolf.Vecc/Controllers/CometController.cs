71fb85f57da2ee19c988bd6e9aca8528121776d1
wolffuSoft <wolffuSoft@126.com>
2019-04-26 17:23:36 +0800

    1

--- Wolf.Vecc/Wolf.Vecc/Controllers/CometController.cs
+++ Wolf.Vecc/Wolf.Vecc/Controllers/CometController.cs
@@ -23,20 +23,17 @@ namespace Wolf.Vecc.Controllers
 
         public void ApprovalPollingAsync()
         {
-            //计时器，10分钟触发一次Elapsed事件600000
             System.Timers.Timer timer = new System.Timers.Timer(5000);
             AsyncManager.OutstandingOperations.Increment();
             timer.Elapsed += (sender, e) =>
             {
                 AsyncManager.Parameters["now"] = _sysUserService.GetApprovalNumber().ToString();
-                //告诉ASP.NET异步操作已完成，进行LongPollingCompleted方法的调用
                 AsyncManager.OutstandingOperations.Decrement();
             };
             //启动计时器
             timer.Start();
         }
 
-        //LongPolling Action 2 - 异步处理完成，向客户端发送响应
         public ActionResult ApprovalPollingCompleted(string now)
         {
             return Json(new
