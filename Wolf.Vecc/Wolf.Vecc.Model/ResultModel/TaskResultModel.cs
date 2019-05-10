6bc30784b7ab56def9195622a1fc3f2937f82e34
wolffuSoft <wolffuSoft@126.com>
2019-05-10 11:25:43 +0800

    人员报表统计

---
+++ Wolf.Vecc/Wolf.Vecc.Model/ResultModel/TaskResultModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wolf.Vecc.Model.ResultModel
+{
+    public class TaskResultModel
+    {
+        public string TaskId{get;set;}
+
+        public string VehicleType { get;set;}
+
+        public string TestDate { get; set; }
+
+        public string TestTime { get; set; }
+
+        public string TestPerson { get; set; }
+
+        public string PlaceTest { get; set; }
+
+        public string RouteDescription { get; set; }
+    }
+}
