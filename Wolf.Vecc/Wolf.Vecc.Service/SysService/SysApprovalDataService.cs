4bd5b8d3e938fb7bbe9b78ce5a3b60d1f3e8ddb5
wolffuSoft <wolffuSoft@126.com>
2019-05-13 17:16:54 +0800

    新增数据查询管理界面

---
+++ Wolf.Vecc/Wolf.Vecc.Service/SysService/SysApprovalDataService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wolf.Vecc.IService.ISysService;
+
+namespace Wolf.Vecc.Service.SysService
+{
+    public class SysApprovalDataService: ISysApprovalDataService
+    {
+
+    }
+}
